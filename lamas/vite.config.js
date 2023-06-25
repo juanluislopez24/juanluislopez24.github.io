13690ac7eb8b7c178a11674a024dcf3fc7f5490e
unknown <juanluislopez24@gmail.com>
2023-06-25 17:39:58 +0200

    test deploy

--- lamas/vite.config.js
+++ lamas/vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/testPage/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
