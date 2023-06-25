1c285545b7dbe4e8202bbaf7a27f9f8707572851
unknown <juanluislopez24@gmail.com>
2023-06-25 18:30:33 +0200

    as

--- lamas/src/router/index.js
+++ lamas/src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
+    path: '/testPage',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
